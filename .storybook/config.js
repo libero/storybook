639d22628bb38ec2247d1c248d0f6b8b45bcfa43
Chris Wilkinson <c.wilkinson@elifesciences.org>
2019-09-25 16:06:42 +0100

    Add theme

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,5 +1,12 @@
-import {configure} from '@storybook/html';
+import {addParameters, configure} from '@storybook/html';
+import theme from './theme';
 
 const context = require.context('../src', true, /\.stories\.js$/);
+
+addParameters({
+    options: {
+        theme: theme,
+    },
+});
 
 configure(context, module);

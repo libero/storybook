17f2907046720392358ce3a8471447964658e17e
Chris Wilkinson <c.wilkinson@elifesciences.org>
2019-09-26 14:02:40 +0100

    Install Accessibility addon

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,5 +1,8 @@
-import {configure} from '@storybook/html';
+import { addDecorator, configure } from '@storybook/html';
+import { withA11y } from "@storybook/addon-a11y";
 
 const context = require.context('../src', true, /\.stories\.js$/);
+
+addDecorator(withA11y);
 
 configure(context, module);

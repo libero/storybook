b1d3f22f3724222c74ee3208f3683fd76f0c2907
Chris Wilkinson <c.wilkinson@elifesciences.org>
2019-09-26 14:51:45 +0100

    Install Twig.js

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,5 +1,16 @@
 import {configure} from '@storybook/html';
+import { twig } from 'twig';
 
 const context = require.context('../src', true, /\.stories\.js$/);
+const twigContext = require.context('../src/patterns', true, /\.twig$/);
+
+twigContext.keys().forEach(key =>
+  twig({
+    allowInlineIncludes: true,
+    data: twigContext(key).tokens,
+    id: key,
+    rethrow: true,
+  })
+);
 
 configure(context, module);
---
+++ .storybook/webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = async ({ config }) => {
+  config.module.rules.push({
+    test: /\.twig$/,
+    use: [
+      {
+        loader: 'twig-loader',
+        options: {
+          allow_async: true,
+          autoescape: true,
+          strict_variables: true,
+        },
+      },
+    ],
+  });
+
+  return config;
+};

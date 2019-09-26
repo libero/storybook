cdc7ef833f213fec9ee2ca6a8d4f5aa4fbe4fecd
Chris Wilkinson <c.wilkinson@elifesciences.org>
2019-09-26 12:33:25 +0100

    Install Viewport addon

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,5 +1,12 @@
-import {configure} from '@storybook/html';
+import { addParameters, configure } from '@storybook/html';
+import { INITIAL_VIEWPORTS } from "@storybook/addon-viewport";
 
 const context = require.context('../src', true, /\.stories\.js$/);
+
+addParameters({
+  viewport: {
+    viewports: INITIAL_VIEWPORTS,
+  },
+});
 
 configure(context, module);

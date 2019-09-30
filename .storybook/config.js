8824598a7e53a0f203689cfa912775c144cd179b
Chris Wilkinson <c.wilkinson@elifesciences.org>
2019-09-30 13:32:17 +0100

    feat: install Viewport addon
    Configured with default provided list

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,14 @@
-import { addDecorator, configure } from '@storybook/html';
+import { addDecorator, addParameters, configure } from '@storybook/html';
+import { INITIAL_VIEWPORTS } from "@storybook/addon-viewport";
 import centered from '@storybook/addon-centered/html';
 
 const context = require.context('../src', true, /\.stories\.js$/);
+
+addParameters({
+  viewport: {
+    viewports: INITIAL_VIEWPORTS,
+  },
+});
 
 addDecorator(centered);
 

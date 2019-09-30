8b87edcfa1f7672cb7f2b6c698113321187b9902
David Moulton <d.moulton@elifesciences.org>
2019-09-30 13:27:00 +0100

    Fix duplication

--- .storybook/config.js
+++ .storybook/config.js
@@ -12,11 +12,4 @@ addParameters({
 
 addDecorator(centered);
 
-import { addDecorator, configure } from '@storybook/html';
-import centered from '@storybook/addon-centered/html';
-
-const context = require.context('../src', true, /\.stories\.js$/);
-
-addDecorator(centered);
-
 configure(context, module);

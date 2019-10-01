3ad18f18f0cde5607014105ca38b9ac8c8cff2a1
Chris Wilkinson <c.wilkinson@elifesciences.org>
2019-10-01 09:55:33 +0100

    feat: install Accessibility addon
    Using default configuration

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,7 @@
 import { addDecorator, addParameters, configure } from '@storybook/html';
 import { INITIAL_VIEWPORTS } from "@storybook/addon-viewport";
 import centered from '@storybook/addon-centered/html';
+import { withA11y } from '@storybook/addon-a11y';
 
 const context = require.context('../src', true, /\.stories\.js$/);
 
@@ -11,5 +12,6 @@ addParameters({
 });
 
 addDecorator(centered);
+addDecorator(withA11y);
 
 configure(context, module);

79e3df1bf3b21d00c260252a323145509cb03b92
David Moulton <d.moulton@elifesciences.org>
2019-10-03 18:18:27 +0100

    Add image pattern template and story

---
+++ src/patterns/atoms/inline/image.stories.js
@@ -0,0 +1,14 @@
+import image from './image.twig';
+
+export default {
+  title: 'Atoms|Inline/Image',
+};
+
+export const Basic = () => image(
+  {
+    image: {
+      src: 'https://picsum.photos/600/400',
+      alt: ''
+    }
+  }
+);

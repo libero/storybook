c31df0ee621392d297efe3369b03bd6da96555cc
David Moulton <davidcmoulton@users.noreply.github.com>
2019-10-04 14:54:04 +0100

    feat: add some basic pattern templates
    Introduces:
    
    * the core macros, in html.twig
    
    * link pattern template and stories
    
    * italic pattern template and stories
    
    * image pattern template and stories
    
    * paragraph pattern template and stories
    
    * heading pattern template and stories

---
+++ src/patterns/molecules/heading/heading.stories.js
@@ -0,0 +1,59 @@
+import heading from './heading.twig';
+
+export default {
+  title: 'Molecules|Heading',
+};
+
+export const h1 = () => heading(
+  {
+    level: 1,
+    text: 'A level 1 heading',
+  },
+);
+
+export const h2 = () => heading(
+  {
+    level: 2,
+    text: 'A level 2 heading',
+  },
+);
+
+export const h3 = () => heading(
+  {
+    level: 3,
+    text: 'A level 3 heading',
+  },
+);
+
+export const h4 = () => heading(
+  {
+    level: 4,
+    text: 'A level 4 heading',
+  },
+);
+
+export const h5 = () => heading(
+  {
+    level: 5,
+    text: 'A level 5 heading',
+  },
+);
+
+export const h6 = () => heading(
+  {
+    level: 6,
+    text: 'A level 6 heading',
+  },
+);
+
+export const h1Linked = () => heading(
+  {
+    level: 1,
+    text: {
+      text: 'Linked heading',
+      attributes: {
+        href: '#',
+      },
+    },
+  },
+);

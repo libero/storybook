b27fb893ce7a31e234f9e3c1f5c2defd2f21b66a
Chris Wilkinson <c.wilkinson@elifesciences.org>
2019-10-08 15:53:19 +0100

    No longer needed

--- src/patterns/includes.js
+++
@@ -1,4 +0,0 @@
-const include = (template, args = {}) => ({ template, arguments: args });
-
-export const italic = (args) => include('./atoms/inline/italic/italic.twig', args);
-export const link = (args) => include('./atoms/inline/link/link.twig', args);

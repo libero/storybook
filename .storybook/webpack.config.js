75b89d5d6d167985fdfee6c9bf1d69297f372bf8
David Moulton <d.moulton@elifesciences.org>
2019-10-10 11:11:56 +0100

    Use sass preset for Storybook

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -12,30 +12,7 @@ module.exports = async ({ config }) => {
           },
         },
       ],
-    }
-  );
-
-  config.module.rules.push(
-    {
-      test: /\.scss$/,
-      use: [
-        'style-loader',
-        {
-          loader: 'css-loader',
-          options: {
-            sourceMap: true,
-            importLoaders: 1,
-          },
-        },
-        {
-          loader: 'sass-loader',
-          options: {
-            sourceMap: true,
-          },
-        },
-      ],
     },
-
   );
 
   return config;

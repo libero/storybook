4efc78c572b4ad760dca20b151ed37c467d7f1a4
David Moulton <d.moulton@elifesciences.org>
2019-10-15 09:50:36 +0100

    Revert unrelated code style change

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,19 +1,17 @@
 module.exports = async ({ config }) => {
-  config.module.rules.push(
-    {
-      test: /\.twig$/,
-      use: [
-        {
-          loader: 'twig-loader',
-          options: {
-            allow_async: true,
-            autoescape: true,
-            strict_variables: true,
-          },
+  config.module.rules.push({
+    test: /\.twig$/,
+    use: [
+      {
+        loader: 'twig-loader',
+        options: {
+          allow_async: true,
+          autoescape: true,
+          strict_variables: true,
         },
-      ],
-    },
-  );
+      },
+    ],
+  });
 
   return config;
 };

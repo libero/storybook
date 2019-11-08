3af7f1eec629ee4c9dbaec386145dcfb9d0f0710
David Moulton <d.moulton@elifesciences.org>
2019-11-08 10:20:26 +0000

    Add sass linting

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,5 +1,11 @@
+/* eslint-disable no-console */
+
 import gulp from 'gulp';
 import jest from 'gulp-jest';
+import postcss from 'gulp-postcss';
+import reporter from 'postcss-reporter';
+import stylelint from 'stylelint';
+import syntaxScss from 'postcss-scss';
 
 const appPath = {
   sassFiles: 'src/**/*.scss',
@@ -7,9 +13,26 @@ const appPath = {
   sassTestsEntryPoint: 'test/sass/sass.test.js',
 };
 
-export const testSass = () => gulp.src(appPath.sassTestsEntryPoint).pipe(jest({ reporter: 'spec' }));
+export const lintSass = () => {
+  const processors = [
+    stylelint(),
+    reporter(
+      {
+        clearMessages: true,
+        throwError: true,
+      },
+    ),
+  ];
 
-export const watchSass = () => gulp.watch([appPath.sassFiles, appPath.sassTests], gulp.series(testSass));
+  return gulp.src(appPath.sassFiles)
+    .pipe(postcss(processors, { syntax: syntaxScss }));
+};
+
+const testSass = () => gulp.src(appPath.sassTestsEntryPoint).pipe(jest({ reporter: 'spec' }));
+
+export const validateSass = gulp.series(lintSass, testSass);
+
+export const watchSass = () => gulp.watch([appPath.sassFiles, appPath.sassTests], gulp.series(validateSass));
 
 export const watch = gulp.parallel(watchSass);
 

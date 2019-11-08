d8f1c5d437969d973b8f443a9bc5aeddcdc82d53
David Moulton <d.moulton@elifesciences.org>
2019-11-08 09:59:02 +0000

    Rename path to avoid built in namespace clashes

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,15 +1,15 @@
 import jest from 'gulp-jest';
 import gulp from 'gulp';
 
-const path = {
+const appPath = {
   sassFiles: 'src/**/*.scss',
   sassTests: 'test/sass/**/*.scss',
   sassTestsEntryPoint: 'test/sass/sass.test.js',
 };
 
-export const testSass = () => gulp.src(path.sassTestsEntryPoint).pipe(jest({ reporter: 'spec' }));
+export const testSass = () => gulp.src(appPath.sassTestsEntryPoint).pipe(jest({ reporter: 'spec' }));
 
-export const watchSass = () => gulp.watch([path.sassFiles, path.sassTests], gulp.series(testSass));
+export const watchSass = () => gulp.watch([appPath.sassFiles, appPath.sassTests], gulp.series(testSass));
 
 export const watch = gulp.parallel(watchSass);
 
